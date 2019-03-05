913099a56ba1b035d47a2372da60022c4bece4ff
bnau <naubertrand@gmail.com>
2019-03-05 14:11:06 +0100

    Revision architecture

--- front/cdb/src/app/app-routing.module.ts
+++ front/cdb/src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CompaniesComponent } from './screens/companies/companies.component';
+import { CompaniesComponent } from './companies/companies.component';
+import { EditCompaniesComponent } from './companies/edit-companies/edit-companies.component';
 
 const routes: Routes = [
 
   { path: 'companies', component: CompaniesComponent },
-  { path: '', redirectTo: '/companies', pathMatch: 'full' }
+  { path: '', redirectTo: '/companies', pathMatch: 'full' },
+  
+  { path: 'companies/:id', component: EditCompaniesComponent },
 ];
 
 @NgModule({
--- front/cdb/src/app/companies/edit-companies/edit-companies.component.ts
+++ front/cdb/src/app/companies/edit-companies/edit-companies.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Company } from 'src/app/companies/company.model';
+import { CompanyService } from 'src/app/companies/company.service';
 
 @Component({
   selector: 'app-edit-companies',
@@ -6,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditCompaniesComponent implements OnInit {
 
-  constructor() { }
+  private company: Company;
+
+  constructor(private readonly companyService: CompanyService) { }
 
   ngOnInit() {
   }
