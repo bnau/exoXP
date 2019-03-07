558a99de2d694c3c444e7c4a5ab764215111cd88
bnau <naubertrand@gmail.com>
2019-03-07 13:14:38 +0100

    Update screen

--- front/cdb/src/app/companies/companies.component.spec.ts
+++ front/cdb/src/app/companies/companies.component.spec.ts
@@ -1,5 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { CompaniesComponent } from './companies.component';
 
 
@@ -10,7 +11,7 @@ describe('CompaniesComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [CompaniesComponent],
-      imports: [HttpClientModule]
+      imports: [HttpClientModule, RouterTestingModule]
     })
       .compileComponents();
   }));
